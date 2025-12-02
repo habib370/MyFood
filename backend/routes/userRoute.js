e0ee07ca225a6f06710379284dd4ade6e0e1e087
habib370 <habibsharkar6@gmail.com>
2025-12-02 23:10:13 +0600

    Add verifyEmail route to userRouter

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 
-import {login,register} from '../controllers/userController.js'
+import {login,register,verifyEmail} from '../controllers/userController.js'
 
 const uesrRouter=express.Router();
 
 uesrRouter.post('/register',register);
 uesrRouter.post('/login',login);
+uesrRouter.post('/verify-email',verifyEmail);
 
 export default uesrRouter;
