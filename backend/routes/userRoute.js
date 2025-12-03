ef7600aa34acea4eaf611cea80cec90acd59361d
habib370 <habibsharkar6@gmail.com>
2025-12-03 22:14:19 +0600

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,11 +1,13 @@
 import express from 'express'
-
-import {login,register,verifyEmail} from '../controllers/userController.js'
+import {authMiddleWare} from '../middleware/auth.js'
+import {login,register,verifyEmail,toChangeName,toChagePass} from '../controllers/userController.js'
 
 const uesrRouter=express.Router();
 
 uesrRouter.post('/register',register);
 uesrRouter.post('/login',login);
 uesrRouter.post('/verify-email',verifyEmail);
+uesrRouter.post('/update-name',authMiddleWare,toChangeName)
+uesrRouter.post('/change-pass',authMiddleWare,toChagePass);
 
 export default uesrRouter;
