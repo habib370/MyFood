7ec4b0d2acb7cbf9efaae4ac3c8c6f69a5dc4d89
habib 370 <habibsharkar6@gmail.com>
2025-12-21 14:31:46 +0600

    error handling

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,11 @@
 import mongoose from 'mongoose';
 //mongodb+srv://habib:habib2304008@cluster0.klvertd.mongodb.net/myFood
 //mongodb://localhost:27017
+
+//const MONGO_URL="mongodb://localhost:27017"
+const MONGO_URL="mongodb+srv://habib:habib2304008@cluster0.klvertd.mongodb.net/myFood"
 export const connectDB = async () => {
-  await mongoose.connect("mongodb://localhost:27017").then(() => {
+  await mongoose.connect(MONGO_URL).then(() => {
     console.log("MongoDB connected");
   });
 }
